94dd80f47020a2ba385d69ac9b301ef5f65e90a4
rajahwu <vmradford@gmail.com>
2023-04-29 10:06:57 -0400

    create login form

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from .config import Config
 from flask_login import LoginManager
-from .models import db, User
+from .db import db
+from .models import User
 
 app = Flask(__name__)
 app.config.from_object(Config)
