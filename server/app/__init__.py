016fb15206fd2dc2f3f46c38bf3d22ee12e4bf5c
rajahwu <vmradford@gmail.com>
2023-04-29 09:45:30 -0400

    create user class

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from .config import Config
+from flask_login import LoginManager
+from .models import db, User
 
 app = Flask(__name__)
+app.config.from_object(Config)
+login = LoginManager(app)
+login.login_view = "session.login"
 
 
 @app.route("/data")
---
+++ server/app/models/user.py
@@ -0,0 +1,21 @@
+from flask_login import UserMixin
+from db import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
+
+    @property
+    def password(self):
+        return self.hashed_password
+    
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    
